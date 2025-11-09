f6118f2191ce8cc85ef755cbc0e787cf4e6cbcb9
Jake Walklate <jakewalklate@protonmail.com>
2025-11-09 18:27:14 +1100

    app name

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,6 +36,10 @@ export default async function RootLayout({
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover" />
         <meta name="theme-color" content={cookieTheme === "dark" ? "black" : "white"} />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <meta name="apple-mobile-web-app-title" content="Zesty" />
+        <meta name="application-name" content="Zesty" />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(websiteStructuredData) }}
